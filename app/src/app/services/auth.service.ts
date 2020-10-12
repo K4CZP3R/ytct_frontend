9f63d0788096f81ee0ba6275608a157f595d5636
K4CZP3R <me@k4czp3r.xyz>
2020-10-12 17:53:50 +0200

    add auth service

---
+++ app/src/app/services/auth.service.ts
@@ -0,0 +1,76 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApiGenericResponse } from '../interfaces/api-generic-response';
+import { CredentialsDict } from '../interfaces/credentials-dict';
+import { RestAuthService } from './rest/rest-auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private restAuth: RestAuthService) { }
+  callback(code: string) {
+    return new Promise((resolve, reject) => {
+      this.restAuth.callback(code)
+        .subscribe(
+          (data: CredentialsDict) => {
+            resolve(data);
+          },
+          (error: HttpErrorResponse) => {
+            let apiResp: ApiGenericResponse = {
+              success: false,
+              message: error.status !== 0 ? error.error.message : error.message,
+              status: error.status !== 0 ? error.error.status : error.statusText
+            }
+            reject(apiResp)
+          }
+        )
+    })
+  }
+  login() {
+    return new Promise((resolve, reject) => {
+      this.restAuth.login()
+        .subscribe(
+          (data: ApiGenericResponse) => {
+            if (data.success)
+              resolve(data.message)
+            else {
+              let apiResp: ApiGenericResponse = {
+                message: data.message,
+                status: data.status,
+                success: data.success
+              };
+              reject(apiResp);
+            }
+          },
+          (error: HttpErrorResponse) => {
+            let apiResp: ApiGenericResponse = {
+              success: false,
+              message: error.status !== 0 ? error.error.message : error.message,
+              status: error.status !== 0 ? error.error.status : error.statusText
+            }
+            reject(apiResp)
+
+          }
+        )
+    })
+  }
+
+  setGoogleCredentials(googleCreds: CredentialsDict) {
+    localStorage.setItem("creds", JSON.stringify(googleCreds));
+  }
+
+  getGoogleCredentials(): string {
+    return localStorage.getItem("creds")
+  }
+
+  isAuthenticated(): boolean {
+    let creds = this.getGoogleCredentials();
+    if (creds === undefined) {
+      return false;
+    }
+
+    return true;
+  }
+}
